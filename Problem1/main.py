0e82e1c550afed1d24a00c5e5ee1c3cb36317a0a
Lynn <wangxinlin@westlake.edu.cn>
2025-02-20 09:40:36 +0800

    完成题目

--- Problem1/main.py
+++ Problem1/main.py
@@ -1,9 +1,13 @@
 # main.py
 
-def calculate_sum():
+def calculate_sum(n):
     # 在此编写代码
+    if n==1 or n==0:
+        return n
+    elif n>1:
+        return calculate_sum(n-1)+n
 
 
 if __name__ == "__main__":
-    result = calculate_sum()
+    result = calculate_sum(50)
     print(f"1+2+...+50的和是：{result}")
